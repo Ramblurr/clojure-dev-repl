ab16f51434e165b9c7e24002dbad2461bf611d3e
Casey Link <unnamedrambler@gmail.com>
2026-05-18 11:06:52 +0200

    add devenv flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "clojure-dev-repl";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    clojure-nix-locker.url = "github:bevuta/clojure-nix-locker";
+    clojure-nix-locker.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    inputs@{
+      clojure-nix-locker,
+      self,
+      devenv,
+      devshell,
+      ...
+    }:
+    let
+      jdk = "jdk25";
+    in
+    devenv.lib.mkFlake ./. {
+      inherit inputs;
+      withOverlays = [
+        devshell.overlays.default
+        devenv.overlays.default
+      ];
+      devShell =
+        pkgs:
+        pkgs.devshell.mkShell {
+          imports = [
+            devenv.capsules.base
+            devenv.capsules.clojure
+          ];
+          packages = [
+            (if self ? packages then self.packages.${pkgs.system}.locker else pkgs.deps-lock)
+          ];
+
+        };
+    };
+}
